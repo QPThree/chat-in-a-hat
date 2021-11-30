d97ad3ba122e1b923e7f8307dbbf6e81e4e47277
Mark Duenas <markgdlduenas@gmail.com>
2021-11-30 12:54:17 -0800

    fixed chat jerkiness

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ChatRoom from "./screens/ChatRoom";
@@ -12,22 +12,17 @@ import { getAuth } from "./firebase";
 
 import RegisterScreen from "./screens/RegisterScreen";
 
-
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
-const auth = getAuth();
 
 export default function App() {
-  console.log(auth);
   return (
     <NavigationContainer>
-
       <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name='Home' component={HomeScreen} />
         <Drawer.Screen name='ChatRoom' component={ChatRoom} />
-        <Drawer.Screen name= "Login" component={LoginScreen} />
+        <Drawer.Screen name='Login' component={LoginScreen} />
       </Drawer.Navigator>
-
     </NavigationContainer>
   );
 }

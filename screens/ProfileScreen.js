cb5431d1189e37c1f90875ab4ff3c69fb30b830f
Mark Duenas <markgdlduenas@gmail.com>
2021-12-01 14:10:05 -0800

    added filter for chat rooms

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,58 +1,55 @@
-import React from 'react'
+import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { auth } from "../firebase";
 
-
 export default function ProfileScreen() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const handleSignOut = async () => {
-        await auth
-            .signOut()
-            .then(() => {
-                navigation.navigate("Login");
-            })
-            .catch((e) => alert(e.message));
-        console.log("AUTH AFTER SIGN OUT:", auth)
-    };
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("Login");
+      })
+      .catch((e) => alert(e.message));
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text>Email:{auth.currentUser?.email} </Text>
-                <TouchableOpacity style={styles.button} onPress={handleSignOut}>
-                    <Text style={styles.buttonText}> Sign Out </Text>
-                </TouchableOpacity>
-            </View>
-            
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Email:{auth.currentUser?.email} </Text>
+        <TouchableOpacity style={styles.button} onPress={handleSignOut}>
+          <Text style={styles.buttonText}> Sign Out </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginHorizontal: 5,
-    },
-    header: {
-        flex: 1,
-        justifyContent: "flex-start",
-        padding: 4,
-        borderBottomColor: "grey",
-        borderBottomWidth: 1,
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-    },
-    button: {
-        backgroundColor: "powderblue",
-        width: "60%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-        marginTop: 40,
-    },
+  container: {
+    flex: 1,
+    marginHorizontal: 5,
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-start",
+    padding: 4,
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "powderblue",
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
 });
